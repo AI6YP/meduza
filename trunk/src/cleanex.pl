e41055fd934225823258c60ddd3c9ec76cbe0b1f
alex.drom@gmail.com <alex.drom@gmail.com@71192f98-7290-ee0e-c3fc-6f7f6a626554>
2013-01-26 06:10:54 +0000

    IRDA mode in cleanex

--- trunk/src/cleanex.pl
+++ trunk/src/cleanex.pl
@@ -3,6 +3,37 @@
 use strict;
 use Digest::CRC qw(crc crc16 crcccitt crcccitt_hex);
 
+sub decode {
+	my $in = shift;
+	$in = sprintf "0x%02X", $in;
+
+	my %T = (
+		'0xFF' => 0,
+		'0xFD' => 1,
+		'0xFB' => 2,
+		'0xF7' => 3,
+		'0xEF' => 4,
+		'0xDF' => 5,
+		'0xBF' => 6,
+		'0x7F' => 7,
+		'0xF5' => 8,
+		'0xEB' => 9,
+		'0xED' => 10,
+		'0xDD' => 11,
+		'0xBB' => 12,
+		'0x77' => 13,
+		'0xB7' => 14,
+		'0x5F' => 15
+	);
+	if (defined $T {$in}) {
+#		print $in . '+';
+	} else {
+		print $in . '-';
+	}
+#	print $T {$in} . '=';
+	return $T {$in};
+}
+
 {
 	die "usage: cleanex.pl logfile\n" unless ($#ARGV == 0);
 	my $nname = $ARGV[0];
@@ -16,6 +47,21 @@ use Digest::CRC qw(crc crc16 crcccitt crcccitt_hex);
 	if (substr ($stream, 0, 2) eq 'FF') { # text mode
 		for my $b (split (' ', $stream)) {
 			push @STREAM, hex ($b);
+		}
+	} elsif (ord (substr ($stream, 7, 1)) == 95) { # binary in irda mode
+		for (my $i = 0; $i < $length; $i = $i + 4) {
+			my $h;
+			my $h2 = decode (ord (substr ($stream, $i + 2, 1)));
+			my $h3 = decode (ord (substr ($stream, $i + 3, 1)));
+			$h  = ($h2 + 16 * $h3);
+#			if ($i < 2000) { printf "%02x ", $h; }
+			push @STREAM, $h;
+
+			my $h0 = decode (ord (substr ($stream, $i,     1)));
+			my $h1 = decode (ord (substr ($stream, $i + 1, 1)));
+			$h  = ($h0 + 16 * $h1);
+#			if ($i < 2000) { printf "%02x ", $h; }
+			push @STREAM, $h;
 		}
 	} else { # binary mode
 		for (my $i = 0; $i < $length; $i++) {
