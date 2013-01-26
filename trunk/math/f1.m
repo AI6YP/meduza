f75d27359abfb78741d9d89832ccc41613979fd6
alex.drom@gmail.com <alex.drom@gmail.com@71192f98-7290-ee0e-c3fc-6f7f6a626554>
2013-01-26 19:32:16 +0000

    ploting code

---
+++ trunk/math/f1.m
@@ -0,0 +1,21 @@
+% $Id$
+% file_in - input stream file name
+
+function f1 (file_in, num_of_elements, element_type)
+	data_in = [];
+	count_ = 0;
+
+	fid_in = fopen (file_in, 'r');
+	if (fid_in < 0)
+		s1 = sprintf ('Uable to open file %s for reading\n', file_in);
+		disp (s1);
+	else
+		[data_in, count_] = fread (fid_in, num_of_elements, element_type);
+	end
+	fclose (fid_in);
+
+	plot (data_in);
+end %function
+
+%!demo
+%! f1 ('../logs/20130125_03.bin.f1', 5000, "int16");
